7499f524b8d47c343913d10df816cc8e8a196565
Petie Deveer <50847801+ItalianSquirel@users.noreply.github.com>
2023-04-03 14:14:25 -0800

    Created installScript.sh

---
+++ installScript.sh
@@ -0,0 +1,26 @@
+sudo apt-get install scratch3 thonny rpi-imager -y
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+wget https://raw.githubusercontent.com/Drewsif/PiShrink/master/pishrink.sh
+chmod +x pishrink.sh
+sudo mv pishrink.sh /usr/local/bin
+npm install node-red-contrib-bme280
+npm install node-red-contrib-camerapi
+npm install node-red-contrib-cpu
+npm install node-red-contrib-dht-sensor
+npm install node-red-contrib-easybotics-air-quality
+npm install node-red-contrib-easybotics-ina219-sensor
+npm install node-red-contrib-easybotics-pca9685
+npm install node-red-contrib-easybotics-vl53l0x
+npm install node-red-contrib-hostip
+npm install node-red-contrib-oled
+npm install node-red-contrib-play-audio
+npm install node-red-contrib-thingspeak42
+npm install node-red-dashboard
+npm install node-red-node-pi-gpio
+npm install node-red-node-pi-neopixel
+npm install node-red-node-ping
+npm install node-red-node-pisrf
+npm install node-red-node-random
+npm install node-red-node-serialport
+npm install node-red-node-smooth
+npm install random-generator_node-red-contrib

530cb7018fe2cec6fc182aedc58a0038cef1ed69
Petie Deveer <50847801+ItalianSquirel@users.noreply.github.com>
2023-04-05 13:23:25 -0800

    Formatted the code better

--- installScript.sh
+++ installScript.sh
@@ -1,5 +1,50 @@
-sudo apt-get install scratch3 thonny rpi-imager npm -y
-bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+#!/bin/bash
+
+# update the package list
+sudo apt-get update
+
+# install VSCode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=armhf] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code
+
+# install pishrink
+sudo apt-get install pishrink
+
+# install Thonny
+sudo apt-get install thonny
+
+# install Minecraft Pi edition
+sudo apt-get install minecraft-pi
+
+# install Python
+sudo apt-get install python3 python3-pip
+
+# install Node.js
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# install NodeRed
+sudo npm install -g --unsafe-perm node-red
+
+# install Scratch
+sudo apt-get install scratch
+
+# install RPi Imager
+sudo apt-get install rpi-imager
+
+# install NPM
+sudo apt-get install npm
+
+# install VLC
+sudo apt-get install vlc
+
+# install Geany
+sudo apt-get install geany
+
+# Install all Required NodeRed Pallet Nodes
 npm install node-red-contrib-bme280
 npm install node-red-contrib-camerapi
 npm install node-red-contrib-cpu

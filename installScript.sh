b62ff98946eb05d92a2e07ef70557122f305dba8
Petie Deveer <50847801+ItalianSquirel@users.noreply.github.com>
2023-04-05 15:48:21 -0800

    Update installScript.sh

--- installScript.sh
+++ installScript.sh
@@ -14,7 +14,7 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 
 # Install Node-RED using npm
-sudo npm install -g --unsafe-perm node-red
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 
 # install Scratch
 sudo apt-get install scratch -y

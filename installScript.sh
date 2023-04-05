f455ea4acfb139b401a96224414fa8aacbb8afa9
Petie Deveer <50847801+ItalianSquirel@users.noreply.github.com>
2023-04-05 15:44:27 -0800

    Update installScript.sh

--- installScript.sh
+++ installScript.sh
@@ -1,17 +1,20 @@
 # update the package list
 sudo apt-get update
 
-# install NPM
-sudo apt-get install npm -y
-
 # install Thonny
 sudo apt-get install thonny -y
 
 # install Python
 sudo apt-get install python3 python3-pip -y
 
-# install NodeRed
-bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) -y
+# Install Node.js
+sudo apt-get install -y nodejs
+
+# Install npm
+sudo apt-get install -y npm
+
+# Install Node-RED using npm
+sudo npm install -g --unsafe-perm node-red
 
 # install Scratch
 sudo apt-get install scratch -y
@@ -23,7 +26,7 @@ sudo apt-get install rpi-imager -y
 sudo apt-get install geany -y
 
 # install xdotool
-sudo apt-get install xdotool -y
+#sudo apt-get install xdotool -y
 
 # Install all Required NodeRed Pallet Nodes
 npm install node-red-contrib-bme280 node-red-contrib-camerapi node-red-contrib-cpu node-red-contrib-dht-sensor node-red-contrib-easybotics-air-quality node-red-contrib-easybotics-ina219-sensor node-red-contrib-easybotics-pca9685 node-red-contrib-easybotics-vl53l0x node-red-contrib-hostip node-red-contrib-oled node-red-contrib-play-audio node-red-contrib-thingspeak42 node-red-dashboard node-red-node-pi-gpio node-red-node-pi-neopixel node-red-node-ping node-red-node-pisrf node-red-node-random node-red-node-serialport node-red-node-smooth random-generator_node-red-contrib -y
